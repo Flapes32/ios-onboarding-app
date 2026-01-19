361ffd7464d4e052cc63b1ad2f7005f3cb0d8061
Dennis Brown <brown_denny@icloud.com>
2026-01-19 20:45:57 +0300

    [ADDED] Xcode project structure with initial iOS app files

---
+++ OnboardingApp/OnboardingAppTests/OnboardingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppTests.swift
+//  OnboardingAppTests
+//
+//  Created by  Apple on 19.01.2026.
+//
+
+import Testing
+@testable import OnboardingApp
+
+struct OnboardingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
